1f1d6fda10a844de6dad68ffc4daaf0488da3f0a
agosGentille <gentilleagostina@gmail.com>
2025-09-29 12:02:44 -0300

    Agrego campos contraseña para el inicio de sesion y el registro. Las validaciones e haran con email y contraseña correctos

--- backend/controllers/AuthController.js
+++ backend/controllers/AuthController.js
@@ -2,14 +2,12 @@ const usuarios = require("../data/usuarios");
 
 // LOGIN
 exports.login = (req, res) => {
-  const { email, nombre } = req.body;
+  const { email, password } = req.body;
 
   // Buscar en el array d usuairos
-  const usuario = usuarios.find(
-    u => u.email === email && u.nombre === nombre
-  );
+  const usuario = usuarios.find(u => u.email === email);
 
-  if (!usuario) {
+  if (!usuario || usuario.password !== password) {
     return res.status(401).json({ error: "Credenciales inválidas" });
   }
 
@@ -28,16 +26,16 @@ exports.login = (req, res) => {
 // REGISTRO TEMPORAL - Los usuarios registrados existen en el servidor mientras este
 //corriendo. Si se reinicia NODE, se pierde todo.
 exports.register = (req, res) => {
-  const { nombre, email } = req.body;
+  const { nombre, email, password } = req.body;
 
-  if (!nombre || !email) return res.status(400).json({ error: "Faltan datos" });
+  if (!nombre || !email || !password) return res.status(400).json({ error: "Faltan datos" });
 
   // Chequea si el usuario ya existe (email ya registrado)
   const existe = usuarios.some(u => u.email === email);
   if (existe) return res.status(400).json({ error: "El usuario ya existe" });
 
   const id = usuarios.length ? usuarios[usuarios.length - 1].id + 1 : 1;
-  const nuevoUsuario = { id, nombre, email };
+  const nuevoUsuario = { id, nombre, email, password };
   usuarios.push(nuevoUsuario);
 
   const token = "fake-token-" + id;
