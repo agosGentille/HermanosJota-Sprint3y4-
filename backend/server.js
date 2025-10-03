6f75af574e25aff3b0789175eeb25ab3e86c70fb
marianoInsa <marianoinsaurralde5@gmail.com>
2025-10-03 16:45:45 -0300

    fix: server.js

--- backend/server.js
+++ backend/server.js
@@ -3,25 +3,30 @@ const app = express();
 const PORT = 4000;
 
 const cors = require("cors");
-const path = require('path');
+const path = require("path");
 
-const authRoutes = require("./routes/authRoutes");
+const authRoutes = require("./routes/authRoutes.js");
 const productosRoutes = require("./routes/productos.js");
 
 app.use(cors()); // permite leer JSON en req.body
-app.use(express.json()); //parsea JSON para que no llegue undefined 
+app.use(express.json()); //parsea JSON para que no llegue undefined
 
 //Middleware global que muestre método y url de la petición (Punto 4 consigna final)
 app.use((req, res, next) => {
   console.log(`[${new Date().toISOString()}] ${req.method} ${req.originalUrl}`);
-  next(); 
+  next();
 });
 
 //rutas
-app.use("/api", authRoutes);   
-app.use('/api/productos', productosRoutes); 
+app.use("/api", authRoutes);
+app.use("/api/productos", productosRoutes);
 
-app.use('/Images', express.static(path.join(__dirname, 'public/images')));
+app.use("/Images", express.static(path.join(__dirname, "public/images")));
+
+// 200
+app.get("/", (req, res) => {
+  res.status(200).send("Bienvenido a la Mueblería Hermanos Jota!");
+});
 
 // 404
 app.use((req, res) => {
@@ -32,12 +37,8 @@ app.use((err, req, res, next) => {
   console.error(err.stack);
   res.status(500).json({ error: "Error interno" });
 });
-// 200
-app.get("/",(req,res)=>{
-    res.status(200)
-    .send("Bienvenido a la Mueblería Hermanos Jota!");
-})
 
-app.listen(PORT,()=>{
-    console.log("Se inició el servidor en el puerto:" + PORT);
-})
+app.listen(PORT, () => {
+  console.log(`Se inició el servidor en el puerto: ${PORT}`);
+  console.log(`URL: http://localhost:${PORT}`);
+});
